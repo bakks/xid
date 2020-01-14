5c547864bed1dc914734b3b950923d9bc1540ef2
Peter Bakkum <pbbakkum@gmail.com>
2020-01-13 22:58:13 -0800

    Fix strconv

--- hostid_js.go
+++ hostid_js.go
@@ -9,7 +9,7 @@ import (
 )
 
 var seed = rand.NewSource(time.Now().UnixNano())
-var id = strconv.ItoA(rand.New(seed).Intn(32768))
+var id = strconv.Itoa(rand.New(seed).Intn(32768))
 
 func readPlatformMachineID() (string, error) {
 	return id, nil
